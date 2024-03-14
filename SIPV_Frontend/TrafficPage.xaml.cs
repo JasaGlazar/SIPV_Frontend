d0a57ff8d1facf78c28fd810b8a9d298983c84a2
nikolazar9 <niko.lazar@student.um.si>
2024-03-14 16:21:33 +0100

    komenzar dodal

--- SIPV_Frontend/TrafficPage.xaml.cs
+++ SIPV_Frontend/TrafficPage.xaml.cs
@@ -7,6 +7,7 @@ public partial class TrafficPage : ContentPage
 	ObservableCollection<TrafficNews> list_novic = new ObservableCollection<TrafficNews>();
 	public TrafficPage()
 	{
+		//sem dodal staticne podatke, ce pa bodo pravi jih samo dodajte v list novic. pa v class se lahko doda kaki property ce je to potrebno
 		InitializeComponent();
 		list_novic.Add(new TrafficNews("Zastoj", "Na stajerski avtocesti je oviran promet med prikljuckoma Domzale in Krtina proti Mariboru. Nastal je zastoj, zamuda do 5 minut.", "https://pomocnik.meblosignalizacija.si/images/prometni-zastoj-1123-144.png", new DateTime(2024,3,14,16,39,02)));
         list_novic.Add(new TrafficNews("Delo na cesti", "Na primorski avtocesti bo zaprt vozni pas med Uncem in Logatcem proti Ljubljani od sobote, od 6. ure, do nedelje predvidoma do 16. ure.", "https://www.lorencic.si/0cache/930aa4f2bc49b4b4c108273459b7a5a2.jpg", new DateTime(2024, 3, 13, 14, 22, 34)));

5878f2b8d4707a6680ea5b23e84c6e5b968dba05
nikolazar9 <niko.lazar@student.um.si>
2024-03-14 16:11:39 +0100

    dodal razred za news in pa naredil trafficnewspage. glede novic sem dodal staticne podatke če pa bodo potem pravi pa se pač dodajo

---
+++ SIPV_Frontend/TrafficNews.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIPV_Frontend
+{
+    class TrafficNews
+    {
+        public string title { get; set; }
+        public string news_description { get; set; }
+        public string picture_url { get; set; }
+
+        public DateTime news_date { get; set; }
+
+        public TrafficNews() { }
+        public TrafficNews(string title,string news_description, string picture_url, DateTime news_date)
+        {
+            this.title = title;
+            this.news_description = news_description;
+            this.picture_url = picture_url;
+            this.news_date = news_date;
+        }
+    }
+}
--- SIPV_Frontend/TrafficPage.xaml.cs
+++ SIPV_Frontend/TrafficPage.xaml.cs
@@ -1,9 +1,19 @@
+using System.Collections.ObjectModel;
+
 namespace SIPV_Frontend.Views;
 
 public partial class TrafficPage : ContentPage
 {
+	ObservableCollection<TrafficNews> list_novic = new ObservableCollection<TrafficNews>();
 	public TrafficPage()
 	{
 		InitializeComponent();
+		list_novic.Add(new TrafficNews("Zastoj", "Na stajerski avtocesti je oviran promet med prikljuckoma Domzale in Krtina proti Mariboru. Nastal je zastoj, zamuda do 5 minut.", "https://pomocnik.meblosignalizacija.si/images/prometni-zastoj-1123-144.png", new DateTime(2024,3,14,16,39,02)));
+        list_novic.Add(new TrafficNews("Delo na cesti", "Na primorski avtocesti bo zaprt vozni pas med Uncem in Logatcem proti Ljubljani od sobote, od 6. ure, do nedelje predvidoma do 16. ure.", "https://www.lorencic.si/0cache/930aa4f2bc49b4b4c108273459b7a5a2.jpg", new DateTime(2024, 3, 13, 14, 22, 34)));
+        list_novic.Add(new TrafficNews("Zastoj", "Na stajerski avtocesti pred predorom Podmilj ter med Krtino in Domzalami proti Ljubljani, skupaj zamuda 5 - 10 minut.", "https://pomocnik.meblosignalizacija.si/images/prometni-zastoj-1123-144.png", new DateTime(2024, 3, 12, 11, 14, 31)));
+        list_novic.Add(new TrafficNews("Nesreca", "Na cesti Murska Sobota - Beltinci je zaprt odsek RakiCan - Lipovci.", "https://pomocnik.meblosignalizacija.si/images/prometna-nesreca-1124-144.png", new DateTime(2024, 3, 10, 17, 29, 33)));
+        list_novic.Add(new TrafficNews("Delo na cesti", "Stajerska avtocesta bo zaprta med Vranskim in Blagovico proti Ljubljani od petka, 15. 3., od 21. ure, do nedelje, 17. 3., predvidoma do 15. ure. Zaprt bo tudi uvoz Trojane v obe smeri. Obvoz bo po regionalni cesti.", "https://www.lorencic.si/0cache/930aa4f2bc49b4b4c108273459b7a5a2.jpg", new DateTime(2024, 3, 10, 8, 12, 24)));
+
+        news_collection.ItemsSource = list_novic;
 	}
 }
